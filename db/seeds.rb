ab2016617045aa3113c99d5f0fa1cd83dc97b75f
mehdizebarjadan <mehdizebarjadan@yahoo.com>
2018-07-30 20:09:54 -0400

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,8 @@ puts "5 skills created"
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "My great service",
     body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati totam dignissimos veniam expedita similique nihil vitae cumque blanditiis itaque vel. Odio expedita eligendi velit aliquid possimus perspiciatis deserunt temporibus quo!",
-    main_image: "http://via.placeholder.com/300x200",
-    thumb_image: "http://via.placeholder.com/600x400"
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/300x200"
   )
 end
 
