7fc466e5ecd603dbcb6d3d14aeac2dd44eaf7397
mehdizebarjadan <mehdizebarjadan@yahoo.com>
2018-08-08 11:20:59 -0400

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    MzViewTool::Renderer.copyright 'Mehdi Zebarjadan', 'All rights reserved'
+  end
 end
