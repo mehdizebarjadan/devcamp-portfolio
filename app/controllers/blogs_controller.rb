6b78abf8afe77ffe13b31ec07bdf4701f0080f48
mehdizebarjadan <mehdizebarjadan@yahoo.com>
2018-08-10 19:09:27 -0400

    Implemented partials for blog, font awesome, and pagination

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,7 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     # byebug
-    @blogs = Blog.special_blogs
+    @blogs = Blog.page(params[:page]).per(5)
     @page_title = "My Portfolio Blog | Mehdi Zeb."
   end
 
