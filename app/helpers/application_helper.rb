d3aca392672f82f78ad16125c5b7d05fae1b66c8
mehdizebarjadan <mehdizebarjadan@yahoo.com>
2018-08-13 09:48:03 -0400

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -58,4 +58,16 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Mehdi Zebarjadan Portfolio", sticky: false, time: 1000)
+  end
 end
