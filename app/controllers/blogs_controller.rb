13a329b34c6dc3765363690eda40dc74c7849d5e
mehdizebarjadan <mehdizebarjadan@yahoo.com>
2018-08-14 17:21:00 -0400

    Implemented icons in breadrumbs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -44,7 +44,7 @@ class BlogsController < ApplicationController
     @blog = Blog.new(blog_params)
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.html { redirect_to @blog, notice: 'Post was successfully created.' }
       else
         format.html { render :new }
       end
@@ -56,7 +56,7 @@ class BlogsController < ApplicationController
   def update
     respond_to do |format|
       if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+        format.html { redirect_to @blog, notice: 'Post was successfully updated.' }
       else
         format.html { render :edit }
       end
@@ -68,7 +68,7 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Post was successfully removed.' }
     end
   end
 
